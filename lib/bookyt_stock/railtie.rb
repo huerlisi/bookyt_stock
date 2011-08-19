f99325d22d9dcdd06104eac60069475f7b853ef0
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-08-19 11:29:32 +0200

    Drop auto-adding to bookyt.engines.

--- lib/bookyt_stock/railtie.rb
+++ lib/bookyt_stock/railtie.rb
@@ -3,10 +3,6 @@ require 'rails'
 
 module BookytStock
   class Railtie < Rails::Engine
-    initializer :after_initialize do |app|
-      app.config.bookyt.engines << 'bookyt_stock'
-    end
-
     config.to_prepare do
       ::Invoice.send :include, BookytStock::Invoice
     end
