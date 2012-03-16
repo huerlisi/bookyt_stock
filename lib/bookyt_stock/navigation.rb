58f7e674ba10c0bbc079b1b07d73adc00a49856d
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-03-16 12:59:09 +0100

    Fix id for stock list navigation item.

--- lib/bookyt_stock/navigation.rb
+++ lib/bookyt_stock/navigation.rb
@@ -3,7 +3,7 @@ module BookytStock
     def setup_bookyt_stock(navigation)
       navigation.item :stocks, t_title(:index, Stock), stocks_path,
                    :if => Proc.new { user_signed_in? } do |stocks|
-        stocks.item :stocks, t_title(:index, Stock), stocks_path, :highlights_on => /\/stocks($|\/[0-9]*($|\/.*))/
+        stocks.item :stock_list, t_title(:index, Stock), stocks_path, :highlights_on => /\/stocks($|\/[0-9]*($|\/.*))/
         stocks.item :new_stock, t_title(:new, Stock), new_stock_path
         stocks.item :write_downs, t_title(:write_downs, Stock), write_downs_stocks_path, :highlights_on => /\/stocks\/write_downs($|\?)/
       end
