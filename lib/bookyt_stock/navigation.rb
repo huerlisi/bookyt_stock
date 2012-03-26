be79cddafeff351796db74b555f6b12e0bdeff0b
Andi Staub (CyT) <andi.staub@cyt.ch>
2012-03-26 11:15:02 +0200

    Updatet navigation to bootstrap2.

--- lib/bookyt_stock/navigation.rb
+++ lib/bookyt_stock/navigation.rb
@@ -1,7 +1,7 @@
 module BookytStock
   module Navigation
     def setup_bookyt_stock(navigation)
-      navigation.item :stocks, t_title(:index, Stock), stocks_path,
+      navigation.item :stocks, t_title(:index, Stock), '#',
                    :if => Proc.new { user_signed_in? } do |stocks|
         stocks.item :stock_list, t_title(:index, Stock), stocks_path, :highlights_on => /\/stocks($|\/[0-9]*($|\/.*))/
         stocks.item :new_stock, t_title(:new, Stock), new_stock_path
